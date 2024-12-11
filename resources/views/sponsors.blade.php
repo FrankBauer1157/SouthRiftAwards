1b865a5236a90338e279bf8b9c8ccd48c51b18c8
Frank Bauer <ruttofrank@gmail.com>
2024-12-11 23:26:37 +0300

    dfgh

--- resources/views/sponsors.blade.php
+++ resources/views/sponsors.blade.php
@@ -44,6 +44,13 @@
                         <a href="{{ route('vote.index') }}" class="inline-block px-8 py-3 text-lg font-bold text-white bg-blue-500 rounded-lg hover:bg-blue-700">
                             Vote Now
                         </a>
+                      
+                    </section>
+                    <section class="text-center">
+                        {{-- vote now button --}}
+                        <a href="{{ route('vote.index') }}" class="inline-block px-8 py-3 text-lg font-bold text-white bg-blue-500 rounded-lg hover:bg-blue-700">
+                            Vote Now
+                        </a>
                         <h2 class="text-3xl font-bold text-blue-600">
                             Our Valued Sponsors
                         </h2>
@@ -51,7 +58,6 @@
                             A heartfelt thank you to all our sponsors for their invaluable support and commitment to the success of the South-Rift Matatu Awards 2024!
                         </p>
                     </section>
-
 
             <!-- Sponsors Grid -->
             <div class="grid grid-cols-2 gap-6 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
